caeabb6ca737dac16c75d41068944963b0dc74c2
Jenny <jenny.kats@aol.com>
2016-12-05 11:28:28 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
 
+  # attr_accessor :first_name, :last_name
+  #
+  # def initialize (first_name, last_name)
+  #   @first_name = first_name
+  #   @last_name = last_name
+  # end
+
+
+  def learn (knowledge_string)
+    @knowledge << knowledge_string
+  end
 end
